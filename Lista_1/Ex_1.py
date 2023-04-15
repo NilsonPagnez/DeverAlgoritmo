9eee64d0bbd98237e991b1246eecd598a705f743
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-04-15 15:45:09 -0300

    exercício 1, 3 e 4

---
+++ Lista_1/Ex_1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+num3 = float(input("Digite outro número: "))
+
+print(f"A média destes números é de {(num1 + num2 + num3) / 3}")
---
+++ Lista_1/Ex_2.py
@@ -0,0 +1,3 @@
+num1 = float(input("Digite um número: "))
+
+print(f"O antecessor de {num1} é {num1 - 1 } e seu sucessor é {num1 + 1}")
